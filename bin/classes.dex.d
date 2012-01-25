2405ffede28b7c1c365933e5d0e7d3918fe3af71
konker <konker@gmail.com>
2012-01-25 23:05:10 +0200

    Removed generated files and added .gitignore

--- bin/classes.dex.d
+++
@@ -1,2 +0,0 @@
-/home/fs/markus/WORKING/android/TaTeTi/bin/classes.dex : \
-/home/fs/markus/WORKING/android/TaTeTi/libs/phonegap-1.3.0.jar \
--- gen/R.java.d
+++
@@ -1,6 +0,0 @@
-/home/fs/markus/WORKING/android/TaTeTi/gen/com/morningwoodsoftware/android/tateti/R.java \
- : /home/fs/markus/WORKING/android/TaTeTi/res/layout/main.xml \
-/home/fs/markus/WORKING/android/TaTeTi/res/values/strings.xml \
-/home/fs/markus/WORKING/android/TaTeTi/res/xml/plugins.xml \
-/home/fs/markus/WORKING/android/TaTeTi/res/xml/phonegap.xml \
-/home/fs/markus/WORKING/android/TaTeTi/AndroidManifest.xml \
